50a0bd1e3fc7f1707e9923bc823c6233e2b76c19
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2022-12-13 10:25:39 +0800

    adding classnames, navbutton animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,61 +1,55 @@
-import { useState } from 'react';
+import { useState } from "react";
+import classNames from "classnames";
 
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const navMenuClass = classNames(
+    "z-50 flex justify-end w-full h-0 opacity-0 transition duration-500 ease-out lg:hidden",
+    {
+      "visible opacity-100 h-auto": isNavOpen,
+      invisible: !isNavOpen
+    }
+  );
 
   function toggleNav() {
     setIsNavOpen((prev: boolean) => !prev);
   }
 
   return (
-    <nav id="navbar" className="fixed top-0 z-10 w-full flex items-center justify-between px-4 sm:px-8 py-3 bg-gray-800">
-      <div className="w-full flex flex-wrap">
-        <div className="lg:w-1/2 w-full flex justify-between">
+    <nav
+      id="navbar"
+      className="fixed top-0 z-10 w-full items-center justify-between"
+    >
+      <div className="w-full flex flex-wrap bg-gray-800 px-8 py-3">
+        <div className="lg:w-1/2 w-full items-center flex justify-between">
           <a
-            className="title-font text-md font-bold inline-block mr-4 py-1 whitespace-no-wrap uppercase text-white"
+            className="title-font text-md font-bold uppercase text-white"
             href="#about"
           >
             Sultani
           </a>
           <button
-            className="cursor-pointer mx-0 text-xl block lg:hidden"
+            className="cursor-pointer mx-0 text-xl block lg:hidden rotate-0 active:rotate-[120deg] duration-200 outline-none focus:outline-none"
             type="button"
             onClick={toggleNav}
           >
-            {isNavOpen ? <AiOutlineClose size={25} color="white" /> : <AiOutlineMenu size={25} color="white" />}
+            {isNavOpen ? (
+              <AiOutlineClose size={25} color="white" />
+            ) : (
+              <AiOutlineMenu size={25} color="white" />
+            )}
           </button>
         </div>
-        <div className={isNavOpen ? "flex w-full justify-end " : "hidden"}>
-          <ul className="flex flex-col w-full list-none mt-2">
-            {[
-              { name: 'About', href: '#about' },
-              { name: 'Projects', href: '#projects' },
-              { name: 'Skills', href: '#skills' },
-              { name: 'Reach Me', href: '#connect' },
-            ].map((item) => (
-              <li className="nav-item w-full" key={item.name}>
-                <a
-                  className="py-2 flex text-xs w-full uppercase font-bold text-white hover:opacity-75 hover:bg-gray-700"
-                  href={item.href}
-                  onClick={toggleNav}
-                ><p className="w-full text-right">{item.name}</p>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div
-          className="lg:flex w-1/2 hidden"
-          id="example-navbar-danger"
-        >
+        <div className="lg:flex w-1/2 hidden" id="example-navbar-danger">
           <ul className="flex w-full justify-end list-none">
             {[
-              { name: 'About', href: '#about' },
-              { name: 'Projects', href: '#projects' },
-              { name: 'Skills', href: '#skills' },
-              { name: 'Reach Me', href: '#connect' },
+              { name: "About", href: "#about" },
+              { name: "Projects", href: "#projects" },
+              { name: "Skills", href: "#skills" },
+              { name: "Reach Me", href: "#connect" },
             ].map((item) => (
               <li className="nav-item" key={item.name}>
                 <a
@@ -69,8 +63,28 @@ function Navbar() {
           </ul>
         </div>
       </div>
+      <div className={navMenuClass}>
+        <ul className="flex flex-col w-full list-none">
+          {[
+            { name: "About", href: "#about" },
+            { name: "Projects", href: "#projects" },
+            { name: "Skills", href: "#skills" },
+            { name: "Reach Me", href: "#connect" },
+          ].map((item) => (
+            <li className="w-full bg-gray-800" key={item.name}>
+              <a
+                className="px-8 py-4 flex text-xs w-full uppercase font-bold text-white hover:opacity-75 hover:bg-gray-700"
+                href={item.href}
+                onClick={toggleNav}
+              >
+                <p className="w-full text-right">{item.name}</p>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
