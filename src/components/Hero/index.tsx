765e20162b96f73a54edeb9d9c470337790348df
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-02-05 08:18:34 +0800

    separate typing effect

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,40 +1,12 @@
-import { useEffect, useState, useMemo } from "react";
 import { AiOutlineArrowRight } from "react-icons/ai";
 
-import profile from "../../assets/images/profile-baru.png";
+import profile from "../../assets/images/profile-baru.webp";
+import profileBlur from "../../assets/images/blurred/profile-baru.webp";
+import Headline from "./Headline";
 import "./Hero.css";
 
 export default function Hero() {
-  const texts = useMemo(() => ["Front End Dev", "Problem Solver"], []);
-  const [text, setText] = useState(texts[0]);
-  const [isTyping, setIsTyping] = useState(true);
-  const [headline, setHeadline] = useState("\u200B");
-  const [counter, setCounter] = useState(0);
-
-  function sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (isTyping && headline !== text) {
-        setHeadline(text.slice(0, headline.length + 1));
-      } else if (isTyping && headline === text) {
-        sleep(2000).then(() => {
-          setIsTyping(false);
-          setCounter((prev) => (prev + 1) % texts.length);
-        });
-      } else if ((!isTyping && headline === text) || !isTyping) {
-        setHeadline(text.slice(0, headline.length - 1));
-        if (headline.length <= 1) {
-          setText(texts[counter]);
-          setIsTyping(true);
-        }
-      }
-    }, 100);
-    return () => clearTimeout(timeout);
-  }, [headline, isTyping, counter, texts, text]);
-
+  console.log("im rerendered")
   return (
     <section
       id="hero"
@@ -45,10 +17,8 @@ export default function Hero() {
           <h1 className="title-font text-2xl text-white opacity-80 ">
             I'm Ahmad Sultani
           </h1>
-          <h1 className="title-font font-semibold text-6xl text-transparent headline">
-            {headline}
-          </h1>
-        <p className="leading-relaxed text-[14px]">
+          <Headline />
+        <p className="leading-relaxed text-[13px]">
           Sometimes an educator, sometimes a developer, but alltimes a learner.
           <br />
           I am really passionate about Website Development, especially in Front End.
@@ -72,9 +42,10 @@ export default function Hero() {
         </div>
       </div>
       <img
-        className="object-center w-[280px] h-fit pb-12 rounded-full sm:pt-4"
+        className="object-center w-[300px] pb-12 rounded-full sm:pt-6"
         src={profile}
         alt="profile"
+        placeholder={profileBlur}
       />
     </section>
   );
