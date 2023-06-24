667f87b637db42bc558bfce9441f5101ee7faffb
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-06-24 21:20:15 +0800

    feat:loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
@@ -5,18 +7,30 @@ import Skills from "./components/Skills";
 import Connect from "./components/Connect";
 import About from "./components/About";
 import ButtonHome from "./components/ButtonHome";
+import Loader from "./components/Loader";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <main className="text-gray-400 bg-gray-900 flex flex-col gap-40">
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Skills />
-      <Connect />
-      <ButtonHome />
-    </main>
+    <>
+      {isLoading && <Loader />}
+      <main className="text-gray-400 bg-gray-900 flex flex-col gap-40">
+        <Navbar />
+        <Hero />
+        <About />
+        <Projects />
+        <Skills />
+        <Connect />
+        <ButtonHome />
+      </main>
+    </>
   );
 }
 
